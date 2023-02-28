a601cfc8a7ccc64e267efdb040e634168be8260e
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-28 16:11:25 +0100

    FEAT new project with vite, appTitle component add

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+
+import AppTitle from './components/AppTitle.vue'
+
+export default{
+  components: {
+    AppTitle,
+  },
+  data() {
+    return {
+      message: 'ciao Mirko'
+    }
+
+  },
+  methods:{
+    sayHello() {
+      console.log('ciao')
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div>
+    <AppTitle></AppTitle>
+    <h2 @click="sayHello">{{message}}</h2>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="title">{{titolo}}</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                titolo: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style>
+    .title{
+        color: aqua;
+    }
+</style>
